d8d9e8529cbeee47b06b13875143c8d1ce3d5ea0
Rurikide <quebec.japonais@gmail.com>
2022-08-30 16:15:47 -0400

    entrypoint.sh mariadb

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -2,6 +2,39 @@
 
 set -euo pipefail
 
-mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-test-db > /dev/null
+
+if [ ! -f "$MYSQL_DATADIR/init_database.sql" ]
+then
+	chown -R mysql:mysql $MYSQL_DATADIR
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql --skip-test-db > /dev/null
+	cat > "$MYSQL_DATADIR/init_databse.sql" << EOF
+
+CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
+FLUSH PRIVILEGES;
+
+EOF
+
+	mysqld --skip-networking=1 &
+	for i in {0..42}; do
+		if mariadb -u root -proot --databse=mysql <<<'SELECT 1;' & > /dev/null
+		then
+			break
+		fi
+		sleep 1
+	done
+
+	if [ "$i" = 42 ]
+	then
+		echo "Error: starting server"
+	fi
+
+	mariadb -u root -proot < "$MYSQL_DATADIR/init_database" && killall mysqld
+fi
+
+echo "Mariadb listening to port 3306"
 
 exec $@
